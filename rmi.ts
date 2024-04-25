e213a5ca84f2a8af5d6da315abdedf5d21579caf
ryudai <bigdragon.06102001@gmail.com>
2024-04-25 14:22:44 +0900

    フィルターを"none"から"<none>"に修正

--- rmi.ts
+++ rmi.ts
@@ -3,7 +3,7 @@ import $ from "https://deno.land/x/dax@0.39.2/mod.ts";
 const result = await $`docker images`.text();
 const ids = [];
 for (const line of result.split("\n")) {
-  if (line.includes("none")) {
+  if (line.includes("<none>")) {
     ids.push(line.split(/\s+/)[2]);
   }
 }
